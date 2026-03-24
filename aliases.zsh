2abe8661774606539447b798993629fd2faa4dee
Adi-Senku69 <vaditya06@gmail.com>
2026-03-24 09:11:23 +0000

    Added btop and eza

--- aliases.zsh
+++ aliases.zsh
@@ -5,3 +5,8 @@ alias lg=lazygit
 alias nf=neofetch
 alias yz=yazi
 alias zj=zellij
+alias bce="$HOME/bloodhound/bloodhound-cli"
+alias be="source '$HOME/opt/BloodHound.py/.venv/bin/activate'"
+alias adpriv="source '$HOME/opt/bloodyAD/.venv/bin/activate'"
+alias be="source '$HOME/opt/Bloodhound.py/.venv/bin/activate'"
+
